1700af2d00d4fe57b4bee16b7506e428965c3308
Nicolò Greggio <nicolo.greggio.1@studenti.unipd.it>
2022-04-02 13:54:10 +0200

    wip improvements

--- assignment-1/src/greg-graph.py
+++ assignment-1/src/greg-graph.py
@@ -1,53 +1,59 @@
-
 import sys
 import os
 import functools
 import time
+import gc
+from math import log
 import heapq
 from heapq import heappop as hpop
+from unicodedata import numeric
+
 
 def prim(g, s):
-    # build Q as heap
-    # since python sorts heap based on list or list of tuples considering the first element, 
-    # must be build a list of keys:node with keys initially inf
-    # print(f'\n\n*******\n<DOING STUFF WITH{g}')
-    inf=float('inf')
-    for i in g:
-        g[i]['parent']=None
-        g[i]['key']=[inf]
-    Q=[(g[i]['key'], i) for i in list(g.keys())]
-    g[s]['key'][0]=0
+  # build Q as heap
+  # since python sorts heap based on list or list of tuples considering the first element,
+  # must be build a list of keys:node with keys initially inf
+  # print(f'\n\n*******\n<DOING STUFF WITH{g}')
+  inf = float('inf')
+  for i in g:
+    g[i]['parent'] = None
+    g[i]['key'] = [inf]
+  Q = [(g[i]['key'], i) for i in list(g.keys())]
+  g[s]['key'][0] = 0
+  heapq.heapify(Q)
+
+  while len(Q) > 0:
+    u = hpop(Q)
+    # print('adjacent of ', u, ' are ', g[u[1]])
+    for v in [
+      item for item in g[u[1]] if item != 'key' and item != 'parent'
+    ]:
+      for key, i in Q:
+        # print(i,'==',v,' and ',int(g[u[1]][v]),' < ', key[0])
+        if i == v and int(g[u[1]][v]) < key[0]:
+          key[0] = int(g[u[1]][v])
+          g[v]['parent'] = u[1]
     heapq.heapify(Q)
 
-    while len(Q)>0:
-        u=hpop(Q)
-        # print('adjacent of ', u, ' are ', g[u[1]])
-        for v in [item for item in g[u[1]] if item!='key' and item !='parent']:
-            for key, i in Q:
-                # print(i,'==',v,' and ',int(g[u[1]][v]),' < ', key[0])
-                if i==v and int(g[u[1]][v])<key[0]:
-                    key[0]=int(g[u[1]][v])
-                    g[v]['parent']=u[1]
-                    
 
 def read_file(f):
-    h={}
-    with open(f) as file:
-        # nodes, edges = file.readline().strip().split(' ')
-        # print(f'There are {nodes} nodes and {edges} edges')
-        file.readline().strip().split(' ')
-        lines = file.readlines()
-        for line in lines:
-            # print(line)
-            e1, e2, w = line.strip().split(' ')
-            # print(f'({e1}, {e2}) {w}')
-            if e1 not in h:
-                h[e1]={}
-            if e2 not in h:
-                h[e2]={}
-            h[e1][e2]=w
-            h[e2][e1]=w
-        return h
+  h = {}
+  with open(f) as file:
+    # nodes, edges = file.readline().strip().split(' ')
+    # print(f'There are {nodes} nodes and {edges} edges')
+    n, m = file.readline().strip().split(' ')
+    lines = file.readlines()
+    for line in lines:
+      # print(line)
+      e1, e2, w = line.strip().split(' ')
+      # print(f'({e1}, {e2}) {w}')
+      if e1 not in h:
+        h[e1] = {}
+      if e2 not in h:
+        h[e2] = {}
+      h[e1][e2] = w
+      h[e2][e1] = w
+  return h, int(n), int(m)
 
 
 # ========================================
@@ -59,19 +65,51 @@ if not path:
 # os.path.relpath()  -> to get relative path
 # os.path.abspath() ...
 # os.getcwd() -> print working dir
-assert(os.path.exists(path)), "Not a valid path! ("+path+")"
+assert (os.path.exists(path)), "Not a valid path! (" + path + ")"
 os.chdir(path)
 
-print("you chose: "+os.getcwd())
-start=time.time()
-graphs=[]
+print("path chosen: " + os.getcwd())
+base_exec = 1000
+graph_sizes = []
+avgs = []
+# print('File\t Avg')
+# print('-'*20)
 for root, dirs, files in os.walk(os.getcwd()):
-    for file in files:
-        if file.endswith(".txt"):
-            # print(os.path.join(root, file))
-            graph=read_file(os.path.join(root, file))
-            prim(graph, '1')
-            graphs.append(graph)
-end=time.time()
+  for file in files:
+    if file.endswith(".txt"):
+      # print(os.path.join(root, file))
+      graph, nodes, edges = read_file(os.path.join(root, file))
+      graph_sizes.append([nodes, edges])
+      num_exec = int(base_exec / nodes)
+      if num_exec == 0:
+          num_exec = 1
 
-print(f'Time: {end-start}, with {len(graphs)} graphs')
+      gc.disable()
+      start = time.perf_counter_ns()
+      for _ in range(num_exec):
+          prim(graph, '1')
+      end = time.perf_counter_ns()
+      gc.enable()
+
+      avg = int((end - start) / num_exec)
+      avgs.append(avg)
+      # print(graphs, '\t', avg)
+      # graphs.append(graph)
+      # mst={k: v['parent'] for k,v in graph.items()}
+      # print('we have ',mst)
+
+ratios = [None] + [
+  round(avgs[i + 1] / avgs[i], 3) for i in range(len(graph_sizes) - 1)
+]
+c_estimates = [
+  round(avgs[i] / log(graph_sizes[i]), 3) for i in range(len(graph_sizes))
+]
+
+print("Size\tTime(ns)\tCostant\t\tRatio")
+print(50 * "-")
+for i in range(len(graph_sizes)):
+  print(graph_sizes[i], avgs[i], '', c_estimates[i], '', ratios[i], sep="\t")
+print(50 * "-")
+print("average c: ", (sum(c_estimates) / len(c_estimates)))
+
+# print(f'Time: {end-start}, with {len(graphs)} graphs')
--- assignment-1/src/greg-graph.rb
+++ assignment-1/src/greg-graph.rb
@@ -1,5 +1,8 @@
 require 'awesome_print'
 require 'set'
+require 'algorithms'
+
+include Containers
 
 class Vertex
   attr_accessor :name, :neighbors, :weights
@@ -46,35 +49,72 @@ class Graph
 
 end
 
+def prim(g, s)
+  inf=1/0.0
+  # g.each{|i| i.merge({:parent => nil, :key => inf})}
+  q=MinHeap.new
+  g.each do |k,v| 
+    v.merge!({:parent => nil, :key => [inf]})
+    q.push(v[:key], k)
+  end
+  ap q.min
+  g['2'][:key][0]=3
+  q.change_key('2', 3)
+  ap q.min
+  
+  # Q=[(g[i]['key'], i) for i in list(g.keys())]
+  # g[s]['key'][0]=0
+  # heapq.heapify(Q)
+
+  # while len(Q)>0
+  #     u=hpop(Q)
+  #     print('adjacent of ', u, ' are ', g[u[1]])
+  #     for v in [item for item in g[u[1]] if item!='key' and item !='parent']
+  #         for key, i in Q
+  #             # print(i,'==',v,' and ',int(g[u[1]][v]),' < ', key[0])
+  #             if i==v and int(g[u[1]][v])<key[0]
+  #                 key[0]=int(g[u[1]][v])
+  #                 g[v]['parent']=u[1]
+  #     heapq.heapify(Q)
+end
 
 
 path='../dataset-1/*'
-# path='../dataset-1/*01_10*'
+path='../dataset-1/*01_10*'
 
-start=Time.now
 
 graphs=[]
+num_exec=1
+
 Dir[path].each do |file|
-  # g = Hash.new { |h,k| h[k] = {} } # 1
+  g = Hash.new { |h,k| h[k] = {} } # 1
   # g=Set.new # 2
-  g=Graph.new
+  # g=Graph.new
   File.foreach(file).drop(1).each do |line|
     e1,e2,w=line.split ' '
     # 1
-    # g[e1][e2]=w
-    # g[e2][e1]=w
+    g[e1][e2]=w
+    g[e2][e1]=w
 
     # 2
     # g.add({[e1,e2].to_set => w})
-    g.add_vertex(e1)
-    g.add_vertex(e1)
-    g.add_edge(e1, e2, w)
+    
+    # 3
+    # g.add_vertex(e1)
+    # g.add_vertex(e1)
+    # g.add_edge(e1, e2, w)
   end
-  graphs << g
+  # graphs << g
+  GC.disable
+  start=Time.now
+  num_exec.times{prim(g,1)}
+  endt=Time.now
+  GC.enable
+  graphs << (endt-start)/num_exec
 end
 
 #ap graphs
 
-endt=Time.now
 
-puts "Time: #{endt-start}, with #{graphs.count} graphs"
+# puts "Time: #{endt-start}, with #{graphs.count} graphs"
+puts "Averages: #{graphs}"
