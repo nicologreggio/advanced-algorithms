9675af4a7d4ddeb4463c27995de31e5eb6896ad7
Nicolo Greggio <nicolo.greggio.1@studenti.unipd.it>
2022-04-03 23:04:12 +0200

    fianlly sth good

--- assignment-1/src/greg-graph.py
+++ assignment-1/src/greg-graph.py
@@ -1,3 +1,4 @@
+from sqlite3 import TimeFromTicks
 import sys
 import os
 import functools
@@ -6,10 +7,41 @@ import gc
 from math import log
 import heapq
 from heapq import heappop as hpop
+from heapq import heappush as hpush
 from unicodedata import numeric
+
+time_for_heap=0
+
+def prim2(g, s):
+  inf = float('inf')
+  for i in g:
+    g[i]['parent'] = None
+    g[i]['key'] = inf
+    g[i]['inQ'] = 1
+  # Q = [(g[i]['key'], i) for i in list(g.keys())]
+  g[s]['key'] = 0
+  Q=[]
+  hpush(Q, (0,s))
+  # heapq.heapify(Q)
+
+  while len(Q) > 0:
+    u = hpop(Q)
+    g[u[1]]['inQ'] = 0
+    for k, v in [item for item in g[u[1]].items() if item[0] != 'key' and item[0] != 'parent' and item[0] != 'inQ']:
+      if g[k]['inQ'] and v < g[k]['key']:
+        g[k]['key']=v
+        hpush(Q, (v, k))
+        g[k]['parent']=u[1]
+
+      """ for key, i in Q:
+        # print(i,'==',v,' and ',int(g[u[1]][v]),' < ', key[0])
+        if i == v and int(g[u[1]][v]) < key[0]:
+          key[0] = int(g[u[1]][v])
+          g[v]['parent'] = u[1] """
 
 
 def prim(g, s):
+  global time_for_heap 
   # build Q as heap
   # since python sorts heap based on list or list of tuples considering the first element,
   # must be build a list of keys:node with keys initially inf
@@ -33,7 +65,11 @@ def prim(g, s):
         if i == v and int(g[u[1]][v]) < key[0]:
           key[0] = int(g[u[1]][v])
           g[v]['parent'] = u[1]
+    s=time.perf_counter_ns()
     heapq.heapify(Q)
+    e=time.perf_counter_ns()
+    time_for_heap+=e-s
+    
 
 
 def read_file(f):
@@ -51,14 +87,14 @@ def read_file(f):
         h[e1] = {}
       if e2 not in h:
         h[e2] = {}
-      h[e1][e2] = w
-      h[e2][e1] = w
+      h[e1][e2] = int(w)
+      h[e2][e1] = int(w)
   return h, int(n), int(m)
 
 
 # ========================================
-# path='../dataset-1'
-path = 0
+path='../dataset-1'
+# path = 0
 if not path:
     path = '.'
 
@@ -71,11 +107,12 @@ os.chdir(path)
 print("path chosen: " + os.getcwd())
 base_exec = 1000
 graph_sizes = []
-avgs = []
+run_times = []
 # print('File\t Avg')
 # print('-'*20)
 for root, dirs, files in os.walk(os.getcwd()):
-  for file in files:
+#   files=sorted(files)
+  for file in sorted(files):
     if file.endswith(".txt"):
       # print(os.path.join(root, file))
       graph, nodes, edges = read_file(os.path.join(root, file))
@@ -87,29 +124,34 @@ for root, dirs, files in os.walk(os.getcwd()):
       gc.disable()
       start = time.perf_counter_ns()
       for _ in range(num_exec):
-          prim(graph, '1')
+          prim2(graph, '1')
       end = time.perf_counter_ns()
       gc.enable()
 
       avg = int((end - start) / num_exec)
-      avgs.append(avg)
+      run_times.append(avg)
       # print(graphs, '\t', avg)
       # graphs.append(graph)
       # mst={k: v['parent'] for k,v in graph.items()}
       # print('we have ',mst)
 
-ratios = [None] + [
-  round(avgs[i + 1] / avgs[i], 3) for i in range(len(graph_sizes) - 1)
-]
-c_estimates = [
-  round(avgs[i] / log(graph_sizes[i]), 3) for i in range(len(graph_sizes))
-]
 
-print("Size\tTime(ns)\tCostant\t\tRatio")
-print(50 * "-")
+""" 
+=================== MEASURE RUN TIME
+"""
+ratios = [None] + [round(run_times[i+1]/run_times[i],3) for i in range(len(graph_sizes)-1)]
+c_estimates = [round(run_times[i]/log(graph_sizes[i][0]*graph_sizes[i][1]),3) for i in range(len(graph_sizes))]
+
+padding=18
+headers=[str(h).ljust(padding) for h in['Size','Time(ns)','Costant','Ratio']]
+hr=padding*(len(headers)+1) * "-"
+print('\n\n\n')
+print(*headers, sep='\t')
+print(hr)
 for i in range(len(graph_sizes)):
-  print(graph_sizes[i], avgs[i], '', c_estimates[i], '', ratios[i], sep="\t")
-print(50 * "-")
+  print(str(graph_sizes[i]).ljust(padding), str(run_times[i]).ljust(padding), str(c_estimates[i]).ljust(padding), str(ratios[i]).ljust(padding), sep="\t")
+print(hr)
 print("average c: ", (sum(c_estimates) / len(c_estimates)))
 
 # print(f'Time: {end-start}, with {len(graphs)} graphs')
+# print('total heap time: ', time_for_heap/(10**9))
