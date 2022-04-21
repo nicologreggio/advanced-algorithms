48977ce74147920f0ee74dd0318f4ad90ca5bc81
FilippoFantinato <fantinato.filippo99@gmail.com>
2022-04-21 08:03:49 +0200

    Added more tests

--- assignment-1/tests/kruskal_naive_tests.py
+++ assignment-1/tests/kruskal_naive_tests.py
@@ -2,10 +2,10 @@ import io
 import math
 import unittest
 
-from algorithms.kruskal_naive import is_acyclic, kruskal
-from graph.graph import read_graph
+from algorithms.kruskal_naive import is_acyclic, kruskal, kruskal_behaviour
+from graph.graph import Graph, read_graph
 
-class KruskalTest(unittest.TestCase):
+class KruskalNaiveTest(unittest.TestCase):
   def test_is_acyclic_true(self):
     g = read_graph(io.StringIO(
     """10 9
@@ -42,9 +42,17 @@ class KruskalTest(unittest.TestCase):
 
     self.assertEqual(expected, actual)
 
-  def test_kruskal_small_graph(self):
+  def test_empty_graph(self):
+    g = Graph()
+
+    actual = kruskal(g)
+    expected = []
+
+    self.assertEqual(expected, actual)
+
+  def test_tree_developed_right_child(self):
     g = read_graph(io.StringIO(
-     """10 9
+    """10 9
       1 2 4993
       2 3 1392
       3 4 8856
@@ -53,17 +61,72 @@ class KruskalTest(unittest.TestCase):
       6 7 -7462
       7 8 6658
       8 9 -976
-      9 10 9698"""
-    ))
+      9 10 9698"""))
 
-    actual_mst = kruskal(g)
-    expected_mst = [(6, 7, -7462), (8, 9, -976), (4, 5, -433), (2, 3, 1392), (1, 2, 4993), (5, 6, 6590), (7, 8, 6658), (3, 4, 8856), (9, 10, 9698)]
+    actual = kruskal(g)
+    expected = [
+      (6, 7, -7462), (8, 9, -976), (4, 5, -433), (2, 3, 1392), (1, 2, 4993), (5, 6, 6590), (7, 8, 6658), (3, 4, 8856), (9, 10, 9698)
+    ]
 
-    actual_size = len(actual_mst)
-    expected_size = g.get_n() - 1
+    self.assertEqual(expected, actual)
 
-    self.assertEqual(expected_mst, actual_mst)
-    self.assertEqual(expected_size, actual_size)
+  def test_basic_graph(self):
+    g = read_graph(io.StringIO(
+    """10 12
+      1 2 4993
+      1 5 2432
+      2 3 1392
+      2 4 4687
+      2 6 -34
+      3 4 8856
+      3 7 844
+      3 8 -433
+      5 9 -432
+      6 9 -7462
+      6 7 442
+      8 10 -976"""))
+
+    actual = kruskal(g)
+    expected = [
+      (6, 9, -7462), (8, 10, -976), (3, 8, -433), (5, 9, -432), (2, 6, -34), (6, 7, 442), (3, 7, 844), (1, 5, 2432), (2, 4, 4687)
+    ]
+
+    self.assertEqual(expected, actual)
+
+  def test_full_connected_graph(self):
+    g = read_graph(io.StringIO(
+    """4 6
+      1 2 -544
+      1 3 455
+      1 4 -12
+      2 3 84
+      2 4 27
+      3 4 -7"""))
+
+    actual = kruskal(g)
+    expected = [
+      (1, 2, -544), (1, 4, -12), (3, 4, -7)
+    ]
+
+    self.assertEqual(expected, actual)
+
+  def test_complexity_behaviour(self):
+    g = read_graph(io.StringIO(
+    """10 9
+      1 2 4993
+      2 3 1392
+      3 4 8856
+      4 5 -433
+      5 6 6590
+      6 7 -7462
+      7 8 6658
+      8 9 -976
+      9 10 9698"""))
+
+    actual = kruskal_behaviour(g.get_n(), g.get_m())
+    expected = g.get_m() * g.get_n()
+
+    self.assertEqual(expected, actual)
 
 
 if __name__ == '__main__':
