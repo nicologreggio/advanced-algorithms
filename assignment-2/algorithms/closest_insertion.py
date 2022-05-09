7dc40dcdc070d91a6d1d303b671f73ebf9d020b5
Nicolò Greggio <nicolo.greggio.1@studenti.unipd.it>
2022-05-09 23:31:31 +0200

    fix bugs & refactor

--- assignment-2/algorithms/closest_insertion.py
+++ assignment-2/algorithms/closest_insertion.py
@@ -1,58 +1,61 @@
+from typing import Tuple
 from graph.graph import Graph, Vertex, Edge
 
 
-def min_triangular(k, C, g: Graph):
+def min_triangular(k: Vertex, C: list[int], g: Graph) -> "Tuple[int,int]":
+    """returns weight of the edge that minimizes "triangleTSP" and the position where the new one should be inserted"""
     min_weight = float("inf")
     for n in range(len(C) - 1):
         i = C[n]
         j = C[n + 1]
-        w_ik = g.get_adj_list_vertex(k)[i] #todo change to get_weight & test
-        w_kj = g.get_adj_list_vertex(k)[j]
-        w_ij = g.get_adj_list_vertex(i)[j]
+        w_ik = g.get_weight(k, i)
+        w_kj = g.get_weight(k, j)
+        w_ij = g.get_weight(i, j)
         tmp = w_ik + w_kj - w_ij
         if tmp < min_weight:
             min_weight = tmp
             pos = n + 1
     return min_weight, pos
 
-def get_dist(k, C, g: Graph):
-    min_distance=float('inf')
-    r=C[0]
-    for c in C:
-        d=g.get_adj_list_vertex(c)[k]
-        if d<min_distance:
-            r=c
-            min_distance=d
 
-    return c
+def get_dist(k: Vertex, C: list[int], g: Graph) -> "int":
+    """Return the circuit-vertex distance between k and C"""
+    assert len(C) > 0, f"Cannot calculate distance between {k} and empty circuit!"
+
+    return min([g.get_weight(h, k) for h in C])
 
 
-def closest_selection(C, G, g):
-    candidates=G-set(C)
-    min_distance=float('inf')
-    r=list(candidates)[0]
+def closest_selection(C: list[int], G: set[int], g: Graph) -> "Vertex":
+    """Select the node according to the closest insertion heuristic"""
+    candidates = G - set(C)
+    assert (
+        len(candidates) > 0
+    ), "{G}\{C} leads to empty set. No candidates available to choose next closest node!"
+
+    min_distance = float("inf")
     for k in candidates:
-        new_dist=get_dist(k, C, g)
-        if new_dist<min_distance: 
-            min_distance=new_dist
-            r=k
+        new_dist = get_dist(k, C, g)
+        if new_dist < min_distance:
+            min_distance = new_dist
+            r = k
 
-    return k
+    return r
 
 
 def closest_insertion(g: Graph, s: Vertex = 1) -> "list[Edge]":
+    """Return the hamiltonian path built using the closest insertion heuristic"""
     d = g.get_adj_list_vertex(s)
     m = min(d, key=d.get)
     G = set(g.get_vertices())
     C = [s, m, s]
-    w = 2 * g.get_adj_list_vertex(s)[m]
 
-    total_len, total_w = g.get_n(), 0
+    total_len, total_w = g.get_n(), 2 * g.get_weight(s, m)
 
     while len(C) <= total_len:
         k = closest_selection(C, G, g)
-        w_min, pos = min_triangular(k, C, g)
+        w, pos = min_triangular(k, C, g)
         C.insert(pos, k)
-        total_w += w_min
+        total_w += w
 
-    return C#, total_w
+    # optionally, total_w (the weight of the circuit) may be returned as well
+    return C
