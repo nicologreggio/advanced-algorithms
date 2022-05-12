6a583728c9a149ded3d5a7ab1b1427260e6fbee5
rigodiletta <96120275+rigodiletta@users.noreply.github.com>
2022-05-12 10:32:27 +0200

    random test fixed

--- assignment-2/tests/random_insertion_test.py
+++ assignment-2/tests/random_insertion_test.py
@@ -5,20 +5,21 @@ from tsp.tsp_file import TSPFileLabel
 from algorithms.constructive_heuristics import random_insertion
 
 class RandomInsertionTest(unittest.TestCase):
-    def test_insertion(self):
+    def test_all_insertion(self):
         path = "tsp_dataset"
         A = read_all(path)
-        print(A)
-        g = A[0][0]
-        n = int(g.get_information(TSPFileLabel.DIMENSION))
-        current = random_insertion(g)
-        current.sort()
-        expected = [1] + list(range(1, n+1))
+        #print(A)
+        for i in range(len(A)): 
+            g = A[i][0]
+            n = int(g.get_information(TSPFileLabel.DIMENSION))
+            current = random_insertion(g)
+            current.sort()
+            expected = [1] + list(range(1, n+1))
 
-        #print(current)
-        #print(expected)
+            #print(current)
+            #print(expected)
 
-        self.assertEqual(expected, current)
+            self.assertEqual(expected, current)
 
 
 if __name__ == "__main__":
