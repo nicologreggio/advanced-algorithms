1dbd4eba8b276618d7910b65c51ecb189d1e1b47
rigodiletta <96120275+rigodiletta@users.noreply.github.com>
2022-05-15 15:37:26 +0200

    Approximation test added

--- assignment-2/tests/random_insertion_test.py
+++ assignment-2/tests/random_insertion_test.py
@@ -3,7 +3,9 @@ import unittest
 from graph.graph import Graph, read_all
 from tsp.tsp_file import TSPFileLabel
 from algorithms.constructive_heuristics import random_insertion
-
+from algorithms.measure_algorithm_performance import compute_approximate_solution
+import math 
+'''
 class RandomInsertionTest(unittest.TestCase):
     def test_all_insertion(self):
         path = "tsp_dataset"
@@ -20,7 +22,20 @@ class RandomInsertionTest(unittest.TestCase):
             #print(expected)
 
             self.assertEqual(expected, current)
+'''
 
+class RandomInsertionApprox(unittest.TestCase):
+    def test_approx(self): 
+        path = "tsp_dataset"
+        A = read_all(path)
+        for i in range(len(A)): 
+            g = A[i][0]
+            n = int(g.get_information(TSPFileLabel.DIMENSION))
+            result = random_insertion(g)
+            approximate_solution = compute_approximate_solution(g, result)
+            optimal_solution = int(g.get_information(TSPFileLabel.OPTIMAL_SOLUTION))
+            err = round((approximate_solution - optimal_solution) / optimal_solution, 4)
+            assert (err <= math.log(n) - 1)
 
 if __name__ == "__main__":
     unittest.main()
