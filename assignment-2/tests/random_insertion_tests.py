0659f5249b5e6268e190895abbb5ed75acf7f43c
FilippoFantinato <fantinato.filippo99@gmail.com>
2022-05-16 10:46:56 +0200

    Merged the two random insertion test classes

--- assignment-2/tests/random_insertion_tests.py
+++ assignment-2/tests/random_insertion_tests.py
@@ -3,38 +3,34 @@ from graph.graph import Graph, read_all
 from tsp.tsp_file import TSPFileLabel
 from algorithms.constructive_heuristics import random_insertion
 from algorithms.measure_algorithm_performance import compute_approximate_solution
-import math 
-'''
+import math
+
+
 class RandomInsertionTest(unittest.TestCase):
     def test_all_insertion(self):
         path = "tsp_dataset"
         A = read_all(path)
-        #print(A)
-        for i in range(len(A)): 
+        for i in range(len(A)):
             g = A[i][0]
             n = int(g.get_information(TSPFileLabel.DIMENSION))
             current = random_insertion(g)
             current.sort()
-            expected = [1] + list(range(1, n+1))
-
-            #print(current)
-            #print(expected)
+            expected = [1] + list(range(1, n + 1))
 
             self.assertEqual(expected, current)
-'''
 
-class RandomInsertionApprox(unittest.TestCase):
-    def test_approx(self): 
+    def test_approx(self):
         path = "tsp_dataset"
         A = read_all(path)
-        for i in range(len(A)): 
+        for i in range(len(A)):
             g = A[i][0]
             n = int(g.get_information(TSPFileLabel.DIMENSION))
             result = random_insertion(g)
             approximate_solution = compute_approximate_solution(g, result)
             optimal_solution = int(g.get_information(TSPFileLabel.OPTIMAL_SOLUTION))
             err = round((approximate_solution - optimal_solution) / optimal_solution, 4)
-            assert (err <= math.log(n) - 1)
+            assert err <= math.log(n) - 1
+
 
 if __name__ == "__main__":
     unittest.main()
