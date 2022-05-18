b95feefafb2f3908da089218f83c32a2a2027c0d
Nicolò Greggio <nicolo.greggio.1@studenti.unipd.it>
2022-05-18 11:41:54 +0200

    udpates tests config for assignment 3

---
+++ assignment-3/tests/testing_tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+class TestingTest(unittest.TestCase):
+    # def __init__(self, sth) -> None:
+    #     super().__init__(sth)
+    #     self.g=Graph(
+    #         [
+    #             (1, 2, 8),
+    #             (1, 3, 6),
+    #             (1, 4, 7),
+    #             (1, 5, 2),
+    #             (2, 3, 9),
+    #             (2, 4, 15),
+    #             (2, 5, 4),
+    #             (3, 4, 12),
+    #             (3, 5, 5),
+    #             (4, 5, 3)
+    #         ]
+    #     )
+
+    def test_circuit_vertex_distance(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
