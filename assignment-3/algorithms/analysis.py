4d659d7ea6f4f2efbd11cbd7f5aa6e8d4cab69d9
FilippoFantinato <fantinato.filippo99@gmail.com>
2022-06-04 19:18:36 +0200

    Updated

--- assignment-3/algorithms/analysis.py
+++ assignment-3/algorithms/analysis.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import gc
 import copy
 
-from algorithms.stoer_wagner import compute_cut_weight 
+from algorithms.stoer_wagner import compute_cut_weight
 
-# ANALYSIS FOR DETERMINISTIC ALGORITHM 
+# ANALYSIS FOR DETERMINISTIC ALGORITHM
+
 
 def d_measure_run_time(alg, graph, num_calls):
     g = copy.deepcopy(graph)
@@ -13,18 +14,15 @@ def d_measure_run_time(alg, graph, num_calls):
     start_time = perf_counter_ns()
 
     for _ in range(num_calls):
-        min_cut = alg(g)
+        w = alg(g)
     end_time = perf_counter_ns()
     gc.enable()
     avg_time = (end_time - start_time) / num_calls
 
-    min_cut_weight = compute_cut_weight(min_cut)
+    return w, avg_time
 
-    return min_cut_weight, avg_time
 
-def d_compute_asymptotic_constant(
-    graphs, alg, asymptotic_behaviour, num_calls=1000
-):
+def d_compute_asymptotic_constant(graphs, alg, asymptotic_behaviour, num_calls=1000):
     run_times = []
     weights = []
     graphs_dimensions = []
@@ -32,21 +30,23 @@ def d_compute_asymptotic_constant(
     ratios = [None]
 
     for graph in graphs:
-        w, run_time = r_measure_run_time(alg, graph, num_calls)
+        w, run_time = d_measure_run_time(alg, graph, num_calls)
         n = graph.get_n()
+        m = graph.get_m()
 
         if run_times:
             ratios.append(round(run_time / run_times[-1], 3))
 
         run_times.append(run_time)
         weights.append(w)
-        graphs_dimensions.append(n)
-        c_estimates.append(round(run_time / asymptotic_behaviour(n), 3))
+        graphs_dimensions.append((n, m))
+        c_estimates.append(round(run_time / asymptotic_behaviour(n, m), 3))
 
     return (run_times, graphs_dimensions, ratios, c_estimates, weights)
 
 
-# ANALYSIS FOR RANDOMIZED ALGORITHM 
+# ANALYSIS FOR RANDOMIZED ALGORITHM
+
 
 def r_measure_run_time(alg, graph, num_calls):
     g = copy.deepcopy(graph)
@@ -54,22 +54,20 @@ def r_measure_run_time(alg, graph, num_calls):
     gc.disable()
     start_time = perf_counter_ns()
 
-    # return mincut and discovery time 
+    # return mincut and discovery time
     for _ in range(num_calls):
-        min_cut, discovery_time = alg(g)
+        min_cut_weight, discovery_time = alg(g)
         discovery_times.append(discovery_time)
 
     end_time = perf_counter_ns()
     gc.enable()
     avg_time = (end_time - start_time) / num_calls
     avg_disc_time = sum(discovery_times) / num_calls
-    min_cut_weight = compute_cut_weight(min_cut)
 
     return min_cut_weight, avg_time, avg_disc_time
 
-def r_compute_asymptotic_constant(
-    graphs, alg, asymptotic_behaviour, num_calls=1000
-):
+
+def r_compute_asymptotic_constant(graphs, alg, asymptotic_behaviour, num_calls=1000):
     run_times = []
     discovery_times = []
     weights = []
@@ -80,6 +78,7 @@ def r_compute_asymptotic_constant(
     for graph in graphs:
         w, run_time, disc_time = r_measure_run_time(alg, graph, num_calls)
         n = graph.get_n()
+        m = graph.get_m()
 
         if run_times:
             ratios.append(round(run_time / run_times[-1], 3))
@@ -87,12 +86,14 @@ def r_compute_asymptotic_constant(
         run_times.append(run_time)
         discovery_times.append(disc_time)
         weights.append(w)
-        graphs_dimensions.append(n)
-        c_estimates.append(round(run_time / asymptotic_behaviour(n), 3))
+        graphs_dimensions.append((n, m))
+        c_estimates.append(round(run_time / asymptotic_behaviour(n, m), 3))
 
-    return (run_times, discovery_times,graphs_dimensions, ratios, c_estimates, weights)
+    return (run_times, discovery_times, graphs_dimensions, ratios, c_estimates, weights)
 
-def plot_run_vs_discovery(run_times,
+
+def plot_run_vs_discovery(
+    run_times,
     discovery_times,
     graphs_dimensions,
     title,
@@ -108,7 +109,9 @@ def plot_run_vs_discovery(run_times,
     plt.legend()
     plt.show()
 
-# COMPLEXITY PLOT 
+
+# COMPLEXITY PLOT
+
 
 def plot_complexity(
     C,
@@ -119,8 +122,8 @@ def plot_complexity(
     expected_graphic_label,
     actual_graphic_label="Obtained complexity",
 ):
-    x = graphs_dimensions
-    reference_z = [C * asymptotic_behaviour(n) for n in graphs_dimensions]
+    x = [n for n, _ in graphs_dimensions]
+    reference_z = [C * asymptotic_behaviour(n, m) for (n, m) in graphs_dimensions]
 
     plt.plot(x, reference_z, label=expected_graphic_label)
     plt.plot(x, run_times, label=actual_graphic_label)
@@ -130,5 +133,3 @@ def plot_complexity(
     plt.ylabel("Time")
     plt.legend()
     plt.show()
-
-
--- assignment-3/algorithms/karger_stein.py
+++ assignment-3/algorithms/karger_stein.py
@@ -5,30 +5,31 @@ from typing import Set, Tuple
 from graph.graph import Graph, Vertex
 from algorithms.stoer_wagner import compute_cut_weight
 
-def karger_stein(g: Graph) -> Tuple[Tuple[Set[Vertex], Set[Vertex]], int]:
+
+def karger_stein(g: Graph) -> Tuple[int, int]:
     k = math.ceil(math.log(g.get_n(), 2) ** 2)
     minimum = float("+inf")
-    #minimum_cut = None
+    # minimum_cut = None
 
     start_time = perf_counter_ns()
     found_time = start_time
     for _ in range(k):
         w = g.recursive_contract()
-        #C, w = g.recursive_contract() # wrong return type?
-        #current_value = compute_cut_weight(g, *C) 
-        #TODO  w == current_value ???
+        # C, w = g.recursive_contract() # wrong return type?
+        # current_value = compute_cut_weight(g, *C)
+        # TODO  w == current_value ???
         if w < minimum:
             minimum = w
-            #minimum_cut = C
+            # minimum_cut = C
             found_time = perf_counter_ns()
 
-        #if current_value < minimum:
+        # if current_value < minimum:
         #    minimum = current_value
         #    minimum_cut = C
         #    found_time = perf_counter_ns()
 
     discovery_time = found_time - start_time
-    #return minimum_cut, discovery_time
+    # return minimum_cut, discovery_time
     return minimum, discovery_time
 
 
