b71a734b4342a1f0897d02d6fe202a5db790611a
rigodiletta <96120275+rigodiletta@users.noreply.github.com>
2022-06-11 16:39:16 +0200

    some plots experiments

--- assignment-3/algorithms/comparison.py
+++
@@ -1,4 +0,0 @@
-# TODO: write new functions to compare algorithms and compute error 
-
-def error_function(approximate_solution, optimal_solution):
-    return round((approximate_solution - optimal_solution) / optimal_solution, 4)
